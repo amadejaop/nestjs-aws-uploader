2980d6032def7c11a62f94f8246f6ad8c756cec7
Amadeja <obsteter.amadeja@gmail.com>
2024-01-19 07:03:21 +0100

    Create upload function to upload to aws
    Created .env file to store credentials and upload rate variables for
    limiting the ttl and number of uploads in given time.
    Created a function that uploads the file to the aws s3.
    Added and used Throttler to add limits to the uploads and prevent brute
    force attacks.

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
 export class UploadController {
+    constructor(private readonly uploadService: UploadService) {}
+
     @Post()
     @UseInterceptors(FileInterceptor('file'))
-    uploadFile(@UploadedFile(
+    async uploadFile(@UploadedFile(
         new ParseFilePipe({
             validators: [
                 /* new MaxFileSizeValidator({ maxSize: 1000 }),
@@ -13,6 +16,7 @@ export class UploadController {
             ],
         }),
     ) file: Express.Multer.File) {
-        console.log(file);
+        // console.log(file);
+        await this.uploadService.upload(file.originalname, file.buffer);
     }
 }
--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ThrottlerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ([{
+        ttl: configService.getOrThrow('UPLOAD_RATE_TTL'),
+        limit: configService.getOrThrow('UPLOAD_RATE_LIMIT'),
+      }]),
+      inject: [ConfigService],
+    }),
+    /* ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 3,
+    }]) */
+  ],
   controllers: [UploadController],
-  providers: [UploadService]
+  providers: [UploadService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+  }],
 })
 export class UploadModule {}
